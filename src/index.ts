a69cfed03013ffa55b4d14e5dba57f71f8e87a4d
Christopher Elias <christopher.elias@mattel.com>
2025-02-11 14:07:30 -0800

    change asset dir for android

--- src/index.ts
+++ src/index.ts
@@ -1,106 +1,112 @@
 import {
-	type ConfigPlugin,
-	IOSConfig,
-	createRunOncePlugin,
-	withDangerousMod,
-	withXcodeProject,
+  type ConfigPlugin,
+  IOSConfig,
+  createRunOncePlugin,
+  withDangerousMod,
+  withXcodeProject,
 } from "@expo/config-plugins";
 import type { ExpoConfig } from "@expo/config-types";
 import { copyFileSync, ensureDirSync, readdir } from "fs-extra";
 import * as path from "node:path";
 
 function withCustomAssetsAndroid(
-	config: ExpoConfig,
-	props: { assetsPaths: string[], ignoredPattern?:string },
+  config: ExpoConfig,
+  props: { assetsPaths: string[]; ignoredPattern?: string }
 ) {
-  const {assetsPaths,ignoredPattern} = props
-	return withDangerousMod(config, [
-		"android",
-		async (config) => {
-			const { projectRoot } = config.modRequest;
-			const resDir = path.join(
-				projectRoot,
-				"android",
-				"app",
-				"src",
-				"main",
-				"res",
-			);
-			const rawDir = path.join(resDir, "raw");
-			ensureDirSync(rawDir);
+  const { assetsPaths, ignoredPattern } = props;
+  return withDangerousMod(config, [
+    "android",
+    async (config) => {
+      const { projectRoot } = config.modRequest;
+      const resDir = path.join(projectRoot, "android", "app", "src", "main");
+      const assetsDir = path.join(resDir, "assets");
+      ensureDirSync(assetsDir);
 
-			for (const assetSourceDir of assetsPaths) {
-				const assetSourcePath = path.join(projectRoot, assetSourceDir);
-				const assetFiles = (await readdir(assetSourcePath)).filter(file => ignoredPattern === undefined ? true : !file.match(new RegExp(ignoredPattern)));
+      for (const assetSourceDir of assetsPaths) {
+        const assetSourcePath = path.join(projectRoot, assetSourceDir);
+        const assetFiles = (await readdir(assetSourcePath)).filter((file) =>
+          ignoredPattern === undefined
+            ? true
+            : !file.match(new RegExp(ignoredPattern))
+        );
 
-				for (const assetFile of assetFiles) {
-					const srcAssetPath = path.join(assetSourcePath, assetFile);
-					const destAssetPath = path.join(rawDir, assetFile);
-					copyFileSync(srcAssetPath, destAssetPath);
-				}
-			}
+        for (const assetFile of assetFiles) {
+          const srcAssetPath = path.join(assetSourcePath, assetFile);
+          const destAssetPath = path.join(assetsDir, assetFile);
+          copyFileSync(srcAssetPath, destAssetPath);
+        }
+      }
 
-			return config;
-		},
-	]);
+      return config;
+    },
+  ]);
 }
 
 function withCustomAssetsIos(
-	config: ExpoConfig,
-	props: { assetsPaths: string[]; assetsDirName?: string, ignoredPattern?:string },
+  config: ExpoConfig,
+  props: {
+    assetsPaths: string[];
+    assetsDirName?: string;
+    ignoredPattern?: string;
+  }
 ) {
-  const {assetsPaths, assetsDirName, ignoredPattern} = props
-	return withXcodeProject(config, async (config) => {
-		const { projectRoot } = config.modRequest;
-		const iosDir = path.join(projectRoot, "ios");
-		const assetsDir = path.join(iosDir, assetsDirName ?? "Assets");
-		ensureDirSync(assetsDir);
+  const { assetsPaths, assetsDirName, ignoredPattern } = props;
+  return withXcodeProject(config, async (config) => {
+    const { projectRoot } = config.modRequest;
+    const iosDir = path.join(projectRoot, "ios");
+    const assetsDir = path.join(iosDir, assetsDirName ?? "Assets");
+    ensureDirSync(assetsDir);
 
-		for (const assetSourceDir of assetsPaths) {
-			const assetSourcePath = path.join(projectRoot, assetSourceDir);
-			// const assetFiles = await readdir(assetSourcePath);
-			const assetFiles = (await readdir(assetSourcePath)).filter(file => ignoredPattern === undefined ? true : !file.match(new RegExp(ignoredPattern)));
+    for (const assetSourceDir of assetsPaths) {
+      const assetSourcePath = path.join(projectRoot, assetSourceDir);
+      // const assetFiles = await readdir(assetSourcePath);
+      const assetFiles = (await readdir(assetSourcePath)).filter((file) =>
+        ignoredPattern === undefined
+          ? true
+          : !file.match(new RegExp(ignoredPattern))
+      );
 
-			const project = config.modResults;
-			const groupName = "Assets";
+      const project = config.modResults;
+      const groupName = "Assets";
 
-			for (const assetFile of assetFiles) {
-				const assetPath = path.join(assetSourceDir, assetFile);
-				const destAssetPath = path.join(assetsDir, assetFile);
-				copyFileSync(assetPath, destAssetPath);
-				IOSConfig.XcodeUtils.addResourceFileToGroup({
-					filepath: destAssetPath,
-					groupName,
-					project,
-					isBuildFile: true,
-					verbose: true,
-				});
-			}
-		}
+      for (const assetFile of assetFiles) {
+        const assetPath = path.join(assetSourceDir, assetFile);
+        const destAssetPath = path.join(assetsDir, assetFile);
+        copyFileSync(assetPath, destAssetPath);
+        IOSConfig.XcodeUtils.addResourceFileToGroup({
+          filepath: destAssetPath,
+          groupName,
+          project,
+          isBuildFile: true,
+          verbose: true,
+        });
+      }
+    }
 
-		return config;
-	});
+    return config;
+  });
 }
 
-const withCustomAssets: ConfigPlugin<{ assetsPaths: string[], assetsDirName?: string, ignoredPattern?:string }> = (
-	config,
-	props,
-) => {
+const withCustomAssets: ConfigPlugin<{
+  assetsPaths: string[];
+  assetsDirName?: string;
+  ignoredPattern?: string;
+}> = (config, props) => {
   // biome-ignore lint/style/noParameterAssign: this is the way :p
-	config = withCustomAssetsIos(config, props);
+  config = withCustomAssetsIos(config, props);
   // biome-ignore lint/style/noParameterAssign: this is the way :p
-	config = withCustomAssetsAndroid(config, props);
-	return config;
+  config = withCustomAssetsAndroid(config, props);
+  return config;
 };
 
 let pkg: { name: string; version?: string } = {
-	name: "expo-custom-assets",
+  name: "expo-custom-assets",
 };
 
 try {
-	pkg = require("expo-custom-assets/package.json");
+  pkg = require("expo-custom-assets/package.json");
 } catch {
-	console.error("Failed to load package.json for expo-custom-assets");
+  console.error("Failed to load package.json for expo-custom-assets");
 }
 
 export default createRunOncePlugin(withCustomAssets, pkg.name, pkg.version);
